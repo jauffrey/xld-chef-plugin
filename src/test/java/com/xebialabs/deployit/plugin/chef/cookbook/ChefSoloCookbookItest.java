b5915fbd2b6e2d6ecf31acf12ab200458b01e7de
Aditya Kalia <aditya.kalia@gmail.com>
2015-12-08 13:18:05 +0530

    DEPL-9123 Chef plugin improvements

--- src/test/java/com/xebialabs/deployit/plugin/chef/cookbook/ChefSoloCookbookItest.java
+++ src/test/java/com/xebialabs/deployit/plugin/chef/cookbook/ChefSoloCookbookItest.java
@@ -1,16 +1,19 @@
 package com.xebialabs.deployit.plugin.chef.cookbook;
 
+import static com.xebialabs.platform.test.TestUtils.createArtifact;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import java.io.IOException;
+import java.util.List;
+import org.junit.Test;
+import com.xebialabs.deployit.deployment.planner.DeltaSpecificationBuilder;
+import com.xebialabs.deployit.plugin.api.flow.Step;
+import com.xebialabs.deployit.plugin.api.udm.Container;
 import com.xebialabs.deployit.plugin.api.udm.Deployed;
 import com.xebialabs.deployit.plugin.api.udm.DeployedApplication;
 import com.xebialabs.deployit.plugin.api.udm.base.BaseDeployableArtifact;
 import com.xebialabs.deployit.plugin.chef.ChefItestBase;
-import com.xebialabs.deployit.plugin.api.udm.Container;
 import com.xebialabs.deployit.test.support.ItestTopology;
-import org.junit.Test;
-
-import java.io.IOException;
-
-import static com.xebialabs.platform.test.TestUtils.createArtifact;
 
 public class ChefSoloCookbookItest extends ChefItestBase {
 
@@ -21,18 +24,38 @@ public class ChefSoloCookbookItest extends ChefItestBase {
     }
 
     @Test
-    public void shouldDeployCheckCookbookFile() throws IOException {
+    public void shouldDeploySingleRecipe() throws IOException {
+        Deployed<?, ?> deployed = getDeployedArtifact(COOKBOOK_FILE);
+        deployed.setProperty("recipe", "test-book::default");
+        assertThat(getSteps(deployed).size(), equalTo(1));
 
-        DeployedApplication deployedManifest = getDeployedArtifact(COOKBOOK_FILE);
-
+        DeployedApplication deployedManifest = newDeployedArtifact("chefCookbook", "1.0", deployed);
         assertInitial(deployedManifest);
-        assertFileExists(getHost(), "/tmp/chef-solo-cookbook-test");
+
+        assertFileExists(getHost(), "/tmp/chef-solo-cookbook-test-default");
     }
 
-    private DeployedApplication getDeployedArtifact(String manifestFile) throws IOException {
+    @Test
+    public void shouldDeployMultipleRecipe() throws IOException {
+        Deployed<?, ?> deployed = getDeployedArtifact(COOKBOOK_FILE);
+        deployed.setProperty("recipe", "test-book::default,test-book::test");
+        assertThat(getSteps(deployed).size(), equalTo(1));
+
+        DeployedApplication deployedManifest = newDeployedArtifact("chefCookbook", "1.0", deployed);
+        assertInitial(deployedManifest);
+
+        assertFileExists(getHost(), "/tmp/chef-solo-cookbook-test-default", "/tmp/chef-solo-cookbook-test");
+    }
+
+    private Deployed<?, ?> getDeployedArtifact(final String manifestFile)
+            throws IOException {
         BaseDeployableArtifact artifact = createArtifact("chefSoloCookbook", "1.0", manifestFile, "chef.CookbookSpec", tempFolder.newFolder());
-        Deployed<?, ?> d = wizard.deployed(artifact, container, "chef.Cookbook");
-        d.setProperty("recipe", "test-book::test");
-        return newDeployedArtifact("chefCookbook", "1.0", d);
+        return wizard.deployed(artifact, container, "chef.Cookbook");
+    }
+
+    private List<Step> getSteps(Deployed<?, ?> deployed) {
+        DeltaSpecificationBuilder specBuilder = createDeltaSpecBuilder(deployed);
+        specBuilder.create(deployed);
+        return tester.resolvePlan(specBuilder.build());
     }
 }
