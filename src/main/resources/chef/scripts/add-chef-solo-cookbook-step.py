450181d6628ed1766b699e57ba212ca56f25ce81
Arpit Aggarwal <aggarwalarpit.89@gmail.com>
2015-12-08 16:14:18 +0530

    DEPL-9813 - Chef plugin improvements

--- src/main/resources/chef/scripts/add-chef-solo-cookbook-step.py
+++
@@ -1,6 +0,0 @@
-#
-context.addStep(steps.os_script(
-    description="Run recipe %s from %s on chef-solo " % (deployed.recipe,deployed.name),
-    script="chef/chef-solo-cookbook",
-    order=deployed.runOrder
-))
